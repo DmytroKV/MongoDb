70990ff610aab839aac020c97471ef3ddc9adf50
DmytroKV <dmytrokocyuba@gmail.com>
2021-01-14 17:08:59 +0200

    Implemented separate post and get using router

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const myUsers = require("../models/myUsers");
+
+//separate router for GET
+const getRoutes = require("../api-routes/get-api");
+//separate router for POST
+const postRoutes = require("../api-routes/post-api");
 
 const PORT = process.env.PORT || 5000;
 const uri =
@@ -18,41 +22,13 @@ mongoose.connection.on("connected", () => {
   console.log("Connected!");
 });
 
-const data = {
-  item: "Another posted item",
-  qty: 1,
-  size: {
-    h: 25,
-    w: 20,
-    uom: "cm",
-  },
-  status: "A",
-};
-
-//instance for saving
-const newMyUsers = new myUsers(data);
-app.post("/save", (req, res) => {
-  //saving data to MongoDB
-  newMyUsers.save((error) => {
-    if (error) {
-      console.log("Error occured while saving!");
-      res.status(400).json({ error });
-    } else {
-      console.log("Data has been saved!");
-      res.status(201).json({ status: "Data has been saved!" });
-    }
-  });
-});
-app.get("/show", (req, res) => {
-  myUsers //usersModel.myUsers
-    .find({})
-    .then((data) => {
-      console.log("Data: ", data);
-      res.json(data);
-    })
-    .catch((error) => {
-      console.log("error: ", error);
-    });
-});
+//Appending GET
+app.use("/get-routes", getRoutes);
+//Appending POST
+app.use("/post-routes", postRoutes);
 
 app.listen(PORT, console.log(`Server listening at the port ${PORT}`));
+
+//Links:
+//http:localhost:500/get-routes/show  - GET
+//http:localhost:500/post-routes/save - POST
