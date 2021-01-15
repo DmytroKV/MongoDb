dfbb67cb27971dfaf729dd2264404fd25e2f3bbb
DmytroKV <dmytrokocyuba@gmail.com>
2021-01-15 13:25:25 +0200

    Modified project files

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const path = require("path");
-require("dotenv").config({
-  path: path.join("", "", "settings.env"),
-});
+require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
@@ -11,18 +9,9 @@ require("./src/models/myUsers");
 require("./src/config/passport");
 
 app.use(bodyParser.json());
-//app.use(express.static(path.join(__dirname, "public")));
-/*
-const routes = require("./src/api-routes/api/users");
-*/
+
 app.use(require("./src/api-routes"));
 
-/*
-//separate router for GET
-const getRoutes = require("./src/api-routes/get-api");
-//separate router for POST
-const postRoutes = require("./src/api-routes/post-api");
-*/
 const PORT = process.env.MY_PORT;
 const uri = process.env.MONGO_DB;
 
@@ -36,16 +25,4 @@ mongoose.connection.on("connected", () => {
   console.log("Connected!");
 });
 
-/*
-//Appending GET
-app.use("/get-routes", getRoutes);
-//Appending POST
-app.use("/post-routes", postRoutes);
-*/
-
 app.listen(PORT, console.log(`Server listening at the port ${PORT}`));
-
-//Links:
-//http:localhost:5000/get-routes/      - GET (main page)
-//http:localhost:5000/get-routes/show  - GET
-//http:localhost:5000/post-routes/save - POST
