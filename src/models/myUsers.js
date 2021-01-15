4fc12c2f627edc79fab3a9eabf6c5654add8ced3
DmytroKV <dmytrokocyuba@gmail.com>
2021-01-15 14:31:25 +0200

    Implemented refresh token

--- src/models/myUsers.js
+++ src/models/myUsers.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 const crypto = require("crypto");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { throws } = require("assert");
 
 const { Schema } = mongoose;
+
+const saltRounds = 10;
 
 const UsersSchema = new Schema({
   email: String,
@@ -11,8 +15,6 @@ const UsersSchema = new Schema({
 });
 
 UsersSchema.methods.setPassword = function (password) {
-  this.salt = crypto.randomBytes(16).toString("hex");
-
   this.salt = crypto.randomBytes(16).toString("hex");
   this.hash = crypto
     .pbkdf2Sync(password, this.salt, 10000, 512, "sha1")
@@ -25,7 +27,19 @@ UsersSchema.methods.validatePassword = function (password) {
     .toString("hex");
   return this.hash === hash;
 };
-
+UsersSchema.methods.generateRefreshJWT = function () {
+  const today = new Date();
+  const expirationDate = new Date(today);
+  expirationDate.setDate(today.getDate() + 3600);
+  return jwt.sign(
+    {
+      email: this.email,
+      id: this._id,
+      exp: parseInt(expirationDate.getTime() / 1000, 10),
+    },
+    "secret"
+  );
+};
 UsersSchema.methods.generateJWT = function () {
   const today = new Date();
   const expirationDate = new Date(today);
@@ -46,6 +60,7 @@ UsersSchema.methods.toAuthJSON = function () {
     _id: this._id,
     email: this.email,
     token: this.generateJWT(),
+    refreshToken: this.generateRefreshJWT(),
   };
 };
 
